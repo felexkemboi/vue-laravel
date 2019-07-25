7c50a9dabdd1210cc514710b10541ce17847a3bc
felexkemboi <felokemboi10@gmail.com>
2019-07-25 22:49:50 +0300

    file location rearrangement

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,42 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-
-//We import and install the VueRouter plugin with Vue.use()
 Vue.use(VueRouter)
 
 import App from './views/App'
 import Hello from './views/Hello'
-import Home from '/views/Home'
+import Home from './views/Home'
 
-
-//pick the configurations from the settings folder
 const router = new VueRouter({
-	mode:'history',
-	routes:[
-	{
-
-		//A Home component that maps to the / route
-		path:'/',
-		name:'home',
-		component:Home
-	},
-	{
-
-		//A Hello component that maps to the /hello route
-		path:'/hello',
-		name:'hello',
-		component:Hello
-	},
-	],
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component: Hello,
+        },
+    ],
 });
 
 const app = new Vue({
-	el:'#app',
-
-	//We make Vue aware of the App
-	components: { App },
-
-	//We inject the router constant into the new Vue application to get access to this.$router and this.$route
-	router,
+    el: '#app',
+    components: { App },
+    router,
 });
