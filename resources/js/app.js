2c670917ce5fa0615c26f9a9309c01ff0169fbf2
felexkemboi <felokemboi10@gmail.com>
2019-07-25 21:55:24 +0300

    baby stages in defining the simple stages in vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,42 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-require('./bootstrap');
 
-window.Vue = require('vue');
+//We import and install the VueRouter plugin with Vue.use()
+Vue.use(VueRouter)
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+import App from './views/App'
+import Hello from './views/Hello'
+import Home from '/views/Home'
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//pick the configurations from the settings folder
+const router = new VueRouter({
+	mode:'history',
+	routes:[
+	{
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+		//A Home component that maps to the / route
+		path:'/',
+		name:'home',
+		component:Home
+	},
+	{
+
+		//A Hello component that maps to the /hello route
+		path:'/hello',
+		name:'hello',
+		component:Hello
+	},
+	],
+});
 
 const app = new Vue({
-    el: '#app',
+	el:'#app',
+
+	//We make Vue aware of the App
+	components: { App },
+
+	//We inject the router constant into the new Vue application to get access to this.$router and this.$route
+	router,
 });
