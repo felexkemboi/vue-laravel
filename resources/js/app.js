db074caedf2297d00873836835f57488c072fab8
felexkemboi <felokemboi10@gmail.com>
2019-07-26 14:53:04 +0300

    final copy of vue-laravel application

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,8 @@ import App from '../assets/js/views/App'
 import Hello from '../assets/js/views/Hello'
 import Home from '../assets/js/views/Home'
 import UsersIndex from '../assets/js/views/UsersIndex'
- 
+import UserEdit from '../assets/js/views/UserEdit'
+
 
 const router = new VueRouter({
     mode: 'history',
@@ -27,6 +28,11 @@ const router = new VueRouter({
             path: '/users',
             name: 'users.index',
             component: UsersIndex,
+        },
+        {
+            path: '/users/:id/edit',
+            name: 'user.edit',
+            component:UserEdit, 
         },
     ],
 });
